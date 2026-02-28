ff2e869748a5f2c3d7bd2521d791213295b8b1dd
Sriram <gajawadasriram@gmail.com>
2026-02-28 18:50:31 +0530

    Force fix API URL

--- Frontend/src/api/axiosInstance.js
+++ Frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL ? `${import.meta.env.VITE_BACKEND_URL}/api` : "http://localhost:5000/api",
+  baseURL: "https://api.busnotify.me",
 });
 
 // Request interceptor to add JWT token
